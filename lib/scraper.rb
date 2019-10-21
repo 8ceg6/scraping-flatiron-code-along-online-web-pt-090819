3c26db4dd098ea0e0e9c77e0a21461e5aa0f81a2
cedric e goddard <8ceg6@comcast.net>
2019-10-21 17:13:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,9 @@ class Scraper
   
   def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      binding.pry
+     
   end 
+ 
  
   def get_courses
   
